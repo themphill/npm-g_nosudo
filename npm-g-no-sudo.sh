e698e7fb0496c65c55aaaf3a456e92cf68016e4b
Tim Hemphill <themphill@ifbyphone.com>
2015-03-04 16:47:24 -0600

    always verbose, no zsh support, better error handling

--- npm-g-no-sudo.sh
+++ npm-g-no-sudo.sh
@@ -3,33 +3,28 @@
 usage()
 {
 cat << EOF
-usage: $0 [-d] [-v]
+usage: $0 [-d]
 
 This script is intended to fix the common problem where npm users
 are required to use sudo to install global packages.
 
-It will backup a list of your installed packages remove all but npm, 
+It will backup a list of your installed packages remove all but npm,
 then create a local directory, configure node to use this for global installs
 whilst also fixing permissions on the .npm dir before, reinstalling the old packages.
 
 OPTIONS:
    -h	Show this message
    -d	debug
-   -v	Verbose
 EOF
 }
 
 
 DEBUG=0
-VERBOSE=0
 while getopts "dv" OPTION
 do
      case $OPTION in
          d)
              DEBUG=1
-             ;;
-         v)
-             VERBOSE=1
              ;;
          ?)
              usage
@@ -40,75 +35,78 @@ done
 
 to_reinstall='/tmp/npm-reinstall.txt'
 
-if [ 1 = ${VERBOSE} ];	then
-	printf "\nSaving list of existing global npm packages\n"
-fi
+printf "\nSaving list of existing global npm packages\n"
+npm -g list --depth=0
 
 #Get a list of global packages (not deps)
 #except for the npm package
 #save in a temporary file.
-npm -g list --depth=0 --parseable --long | cut -d: -f2 | grep -v '^npm@\|^$' >$to_reinstall
+npm -g list --depth=0 --parseable --long | cut -d: -f2 | grep -v '^npm@\|^$' > $to_reinstall
 
-if [ 1 = ${VERBOSE} ];	then
-	printf "\nRemoving existing packages temporarily - you might need your sudo password\n\n"
-fi
+printf "\nRemoving existing packages temporarily - you might need your sudo password\n\n"
+
 #List the file
 #replace the version numbers
 #remove the newlines
 #and pass to npm uninstall
-
 uninstall='sudo npm -g uninstall'
-if [ 1 = ${DEBUG} ];	then
-	printf "Won't uninstall\n\n"
+if [ 1 = $DEBUG ]; then
+	printf "Debug mode: won't run uninstall\n\n"
+
 	uninstall='echo'
 fi
 if [ -s $to_reinstall ]; then
 	cat $to_reinstall | sed -e 's/@.*//' | xargs $uninstall
 fi
 
-defaultnpmdir="${HOME}/.npm-packages"
-npmdir=''
+#roll back changes if uninstall fails
+if [ $? -ne 0 ]; then
+    printf "\nSome npm packages could not in be uninstalled. Attempting to roll back changes.\n\n"
 
-read -p "Choose your install directory. Default (${defaultnpmdir}) : " npmdir
+    cat $to_reinstall | xargs sudo npm -g install
+    rm $to_reinstall
 
-if [ -z ${npmdir} ]; then
-    npmdir=${defaultnpmdir}
-else
-    if [ ! -d ${npmdir} ]; then
-        echo "${npmdir} is not a directory."
-        exit
-    fi
-    npmdir="${npmdir}/.npm-packages"
+    exit 1
 fi
 
-if [ 1 = ${VERBOSE} ];	then
-	printf "\nMake a new directory ${npmdir} for our "-g" packages\n"
-fi
+oldnpmdir="/usr/local/lib/node_modules"
+npmdir="$HOME/.npm-packages"
 
-if [ 0 = ${DEBUG} ];	then
-	mkdir -p ${npmdir}
+printf "\nMake a new directory $npmdir for our "-g" packages\n"
+
+if [ 0 = $DEBUG ]; then
+	mkdir -p $npmdir
 	npm config set prefix $npmdir
 fi
 
-if [ 1 = ${VERBOSE} ];	then
-	printf "\nFix permissions on the .npm directories\n"
-fi
+printf "\nFix permissions on the .npm directories\n"
 
 me=`whoami`
 sudo chown -R $me ~/.npm
 
-if [ 1 = ${VERBOSE} ];	then
-	printf "\nReinstall packages\n\n"
-fi
+printf "\nReinstall packages\n\n"
 
 #list the packages to install
 #and pass to npm
 install='npm -g install'
-if [ 1 = ${DEBUG} ];	then
+if [ 1 = $DEBUG ]; then
+    printf "Debug mode: won't run install\n\n"
+
 	install='echo'
 fi
 if [ -s $to_reinstall ]; then
 	cat $to_reinstall | xargs $install
+fi
+
+#roll back changes if reinstall fails
+if [ $? -ne 0 ]; then
+    printf "\nSome npm packages could not in be re-installed. Attempting to roll back changes.\n\n"
+
+    npm config set prefix $oldnpmdir
+    cat $to_reinstall | xargs sudo $install
+    rm $to_reinstall
+
+    exit 1
 fi
 
 envfix='
@@ -121,30 +119,19 @@ unset MANPATH  # delete if you already modified MANPATH elsewhere in your config
 MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
 '
 
-fix_env() {
-	if [ -f "${HOME}/.bashrc" ];	then
-		printf "${envfix}" ${npmdir} >> ~/.bashrc
-		printf "\nDon't forget to run 'source ~/.bashrc'\n"
-	fi
-	if [ -f "${HOME}/.zshrc" ];	then
-		printf "${envfix}" ${npmdir} >> ~/.zshrc
-		printf "\nDon't forget to run 'source ~/.zshrc'\n"
-	fi
+printf "\nUpdate .bashrc with the paths and manpaths\n\n"
 
-}
+#create .bashrc if need be, then add path info
+if [ ! -e ~/.bashrc ] || ! grep -q 'NPM_PACKAGES="%s"' ~/.bashrc; then
+    printf "$envfix" $npmdir >> ~/.bashrc
+fi
 
-echo_env() {
-	printf "\nYou may need to add the following to your ~/.bashrc / .zshrc file(s)\n\n" 
-	printf "${envfix}\n\n" ${npmdir} 
-}
+#make sure .bashrc is getting sourced in bash_profile
+if ! grep -q 'source ~/.bashrc' ~/.bash_profile; then
+    echo 'source ~/.bashrc' >> ~/.bash_profile
+fi
 
-printf "\n\n"
-read -p "Do you wish to update your .bashrc/.zshrc file(s) with the paths and manpaths? [yn] " yn
-case $yn in
-    [Yy]* ) fix_env;;
-    [Nn]* ) echo_env;;
-    * ) echo "Please answer 'y' or 'n'.";;
-esac
+source ~/.bash_profile
 
 rm $to_reinstall
 
